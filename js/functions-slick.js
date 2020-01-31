e5722154a4c9521f50a84aa4e762ba7a0f43a4cb
MacBook <macbook@zef-2.local>
2020-01-31 22:39:51 +0200

    Update 2020-01-31T20:39:50.053Z

--- js/functions-slick.js
+++ js/functions-slick.js
@@ -1,33 +1,3 @@
-
-jQuery(function ($) {
-    function fix_size() {
-        var images = $('.slider__one-img');
-        images.each(setsize);
-
-        function setsize() {
-            var img = $(this),
-                img_dom = img.get(0),
-                container = img.parents('.slider__one-container');
-            if (img_dom.complete) {
-                resize();
-            } else img.one('load', resize);
-
-            function resize() {
-                if ((container.width() / container.height()) < (img_dom.width / img_dom.height)) {
-                    img.width('100%');
-                    img.height('auto');
-                    return;
-                }
-                img.height('100%');
-                img.width('auto');
-            }
-        }
-    }
-    $(window).on('resize', fix_size);
-    fix_size();
-});
-
-
 $('.slider-for').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
